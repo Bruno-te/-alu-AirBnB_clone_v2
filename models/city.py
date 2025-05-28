f63571dfc323dedceccc3f41f71d75753ac9f9ff
Bruno-te <i.bruno@alustudent.com>
2025-05-28 22:05:30 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""City model"""
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """City class"""
-
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
