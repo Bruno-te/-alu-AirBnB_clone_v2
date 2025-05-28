509d04206bf15fff72c5e266d3184551a22294c6
Bruno-te <i.bruno@alustudent.com>
2025-05-28 22:03:18 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,47 @@
-#!/usr/bin/python3
-"""BaseModel module"""
-
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
+
+Base = declarative_base()
 
 class BaseModel:
-    """Defines common attributes/methods for other classes."""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Initialize instance"""
         if kwargs:
-            for key, val in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    val = datetime.fromisoformat(val)
+            for key, value in kwargs.items():
                 if key != '__class__':
-                    setattr(self, key, val)
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.utcnow()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.utcnow()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-
-    def __str__(self):
-        """String representation"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
-        """Updates the updated_at timestamp"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary representation of the instance"""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        dict_repr = self.__dict__.copy()
+        dict_repr['__class__'] = self.__class__.__name__
+        if 'created_at' in dict_repr:
+            dict_repr['created_at'] = self.created_at.isoformat()
+        if 'updated_at' in dict_repr:
+            dict_repr['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dict_repr:
+            del dict_repr['_sa_instance_state']
+        return dict_repr
+
+    def delete(self):
+        models.storage.delete(self)
