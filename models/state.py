c4ed2c154251df7c4f96b83354851583e8ed02ae
Bruno-te <i.bruno@alustudent.com>
2025-05-28 22:06:36 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""State model"""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import models
+from models.city import City
 
-from models.base_model import BaseModel
+class State(BaseModel, Base):
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
 
-
-class State(BaseModel):
-    """State class"""
-
-    name = ""
+    @property
+    def cities(self):
+        if models.storage_type != "db":
+            from models import storage
+            return [city for city in storage.all(City).values() if city.state_id == self.id]
+        return self.cities
